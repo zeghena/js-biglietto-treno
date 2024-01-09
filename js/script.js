a0f888c307a4eee9135ef67668aae7ecc584e32f
davidemarchini <marchini73@gmail.com>
2024-01-09 16:56:42 +0100

    km-age

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const resultElement = document.getElementById('ticket');
+
+// ###chiedo all'utente il numero di chilometri che vuole percorrere 
+
+const age = prompt ('Quanti anni hai?')
+console.log(age)
+
+// ### chiedo l'età del passeggero.
+
+const kmtrip = prompt ('Quanti km devi percorrere?')
+console.log(kmtrip)
