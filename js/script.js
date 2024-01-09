0c8c54ba1e15d47da1c2a061625031b0d687ec5f
davidemarchini <marchini73@gmail.com>
2024-01-09 17:34:59 +0100

    discount

--- js/script.js
+++ js/script.js
@@ -2,10 +2,39 @@ const resultElement = document.getElementById('ticket');
 
 // ###chiedo all'utente il numero di chilometri che vuole percorrere 
 
-const age = prompt ('Quanti anni hai?')
-console.log(age)
+const age = prompt ('Quanti anni hai?');
+console.log(age);
 
 // ### chiedo l'età del passeggero.
 
-const kmtrip = prompt ('Quanti km devi percorrere?')
-console.log(kmtrip)
+const kmtrip = prompt ('Quanti km devi percorrere?');
+console.log(kmtrip);
+
+// ### calcolo il prezzo  PARZIALE del biglietto, considerando km * 0,21 euro
+
+const partialticket = kmtrip * 0.21;
+console.log(partialticket)
+
+// ### CALCOLO DELLO SCONTO
+
+let sconto20 = partialticket * 0.20
+console.log=(sconto20)
+
+let sconto40 = partialticket * 0.40
+console.log=(sconto40)
+
+// ### calcolo il prezzo REALE del biglietto
+
+let priceticket = 0;
+
+if (age < 18) {
+    priceticket = partialticket - sconto20;
+} else if (age > 65) {
+    priceticket = partialticket - sconto40;
+}
+
+document.getElementById ('prezzo').innerHTML = ticket;
+
+
+
+
